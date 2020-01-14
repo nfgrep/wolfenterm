8ad2f94cf01e9450b1016c414b2a48e046f15bbf
nfgrep <nfg2600@gmail.com>
2020-01-14 15:03:06 -0500

    Added steep line detect in raycasting

--- src/main.c
+++ src/main.c
@@ -11,7 +11,11 @@ int main()
     player p1;
     int ray_results[NUM_RAYS];
 
-    p1.rot = 0.78539816339; // pi/4 - 45deg in rad
+    //p1.rot = 0.78539816339; // pi/4 - 45deg in rad
+    p1.rot = 1.0471975512; // pi/3 - 60deg in rad, steeper than pi/4
+    //p1.rot = 1.308996939; // pi/3 - 60deg in rad, steeper than pi/4
+    //p1.rot = 1.57079632679; // pi/3 - 60deg in rad, steeper than pi/4
+    //p1.rot = 0.26179938779; // pi/12 - shallow
 
     //Reading map data from file and storing in data member of map struct
     if(read_file("../map.txt", &p1, &m1))
--- src/raycasting.c
+++ src/raycasting.c
@@ -9,19 +9,43 @@ int cast_ray(int* ray_results, map* m1, player* p1)
 {
     double slope = tan(p1->rot);
     double err = 0.0;
+    double abslope = fabs(slope);
     int y = p1->y, x = p1->x;
+    int y_step = (p1->rot > M_PI) ? -1 : 1;
+    char steep = (abslope >= 1) ? 1 : 0;
     printf("cur_player: %d,%d - %f\n",p1->x,p1->y,p1->rot);
     printf("slope: %f\n",slope);
-    while(m1->data[y][x])
+    //If steepline, swap x and y
+    if(steep)
     {
-        err += slope;
-        if(err >= 0.5)
+        int tmp = x;
+        x = y;
+        y = tmp;
+        while(m1->data[x][y])
         {
-            y++;
-            err -= 1.0;
+            err += abslope;
+            if(err >= 0.5)
+            {
+                y+=y_step;
+                err -= 1.0;
+            }
+            x++;
+            printf("Steep: Cur Char: %c\n",m1->data[x][y]);
         }
-        x++;
-        printf("Cur Char: %c\n",m1->data[y][x]);
+    }
+    else
+    {
+        while(m1->data[y][x])
+        {
+            err += abslope;
+            if(err >= 0.5)
+            {
+                y+=y_step;
+                err -= 1.0;
+            }
+            x++;
+            printf("Cur Char: %c\n",m1->data[y][x]);
+        }
     }
     return 0;
 }
